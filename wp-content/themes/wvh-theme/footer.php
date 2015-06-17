72aed6e4e899f5de99b9f4b0399a4fcad20f201f
robbiesh <sherrard.rob@gmail.com>
2015-06-17 16:07:10 -0400

    footer text

--- wp-content/themes/wvh-theme/footer.php
+++ wp-content/themes/wvh-theme/footer.php
@@ -4,15 +4,15 @@
         <div class="col-md-3">
           <div class="footer-block">
             <img src="<?php echo get_template_directory_uri(); ?>/img/online-catalog.png" class="alignleft">
-        		<p>Get your West Virginia Encyclopedia and DVD in our <a href="<?php echo get_permalink( get_page_by_id(275) ) ?>" target="_blank">Online Store &raquo;</a></p>
+        		<p>Get your West Virginia Encyclopedia and DVD in our <a href="<?php echo get_permalink( get_page_by_title('Online Store') ); ?>" target="_blank">Online Store &raquo;</a></p>
           </div>
         </div>
         <div class="col-md-3 middle">
           <div class="footer-block">
             <ul>
   	        	<li><a href="http://www.wvencyclopedia.org/" target="_blank">e-WV: The West Virginia Encyclopedia &raquo;</a></li>
-  	        	<li><a href="<?php echo get_permalink( get_page_by_id(267) ); ?>">Our Historic House &raquo;</a></li>
-  	        	<li><a href="<?php echo get_permalink( get_page_by_id(221) ); ?>">Contact Us &raquo;</a></li>
+  	        	<li><a href="<?php echo get_permalink( get_page_by_title('Our Historic Headquarters') ); ?>">Our Historic House &raquo;</a></li>
+  	        	<li><a href="<?php echo get_permalink( get_page_by_title('Contact Us') ); ?>">Contact Us &raquo;</a></li>
   	        </ul>
           </div>
         </div>
@@ -24,6 +24,7 @@
           <div class="search-form">
             <?php get_search_form( ); ?>
           </div>
+          <small>&copy; <?php echo date("Y"); ?> All Rights Reserved</small>
         </div>
     </div>
   </div>
